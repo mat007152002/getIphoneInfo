3847abdb8cd916132764da2b9a938007920e01bb
Matthew Ling <mat007152002g@gmail.com>
2020-05-26 09:57:40 +0800

    Initial Commit

---
+++ getIphoneInfo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  getIphoneInfo
+//
+//  Created by 旌榮 凌 on 2020/5/26.
+//  Copyright © 2020 旌榮 凌. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
